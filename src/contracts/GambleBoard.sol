1fc9962d0f4aa8d03ee1b0ba17501d1ae99c87ce
LauriTahvanainen <48516187+LauriTahvanainen@users.noreply.github.com>
2021-05-02 14:52:59 +0200

    Function for depositing arbitration fee and creating dispute. Refactoring.

--- src/contracts/GambleBoard.sol
+++ src/contracts/GambleBoard.sol
@@ -1,4 +1,3 @@
-
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.8.0 <0.9.0;
@@ -14,36 +13,43 @@ import "./dependencies/Arbitrable.sol";
  * open:              0
  * waiting for votes: 1
  * resolved:          2
- * disputed:          3
+ * disagreement:      3
+ * disputed:          4
+ * arbitration:       5
  */
 contract GambleBoard is Arbitrable {
     
     uint8 constant STATE_OPEN = 0;
     uint8 constant STATE_VOTING = 1;
     uint8 constant STATE_RESOLVED = 2;
-    uint8 constant STATE_DISPUTED = 3;
+    uint8 constant STATE_DISAGREEMENT = 3;
+    uint8 constant STATE_DISPUTED = 4;
+    uint8 constant STATE_ARBITRATION = 5;
+
+    enum RulingOptions {RefusedToArbitrate, creatorWins, backerWins}
+    uint constant RULING_OPTIONS_AMOUNT = 2; // 0 if can't arbitrate
     
     uint8 constant MAX_COUNTRIES = 200;
     // Odds with 6 decimals.
     uint constant MIN_ODD = 1000000;
     uint constant MIN_STAKE = 1000000;
-    uint constant MIN_TIME_TO_VOTE = 86400;
     
-    enum RulingOptions {RefusedToArbitrate, creatorWins, backerWins}
-    uint constant amountOfRulingOptions = 2; // 0 if can't arbitrate
-    
-    // Indexed params can be filtered in the UI.
-    event BetCreated(bytes indexed betID, bytes betIDData, uint8 indexed country, uint16 indexed category, uint8 league, uint backerStake);
+    uint constant ONE_DAY = 86400;
 
+    // Indexed params can be filtered in the UI.
+    event BetCreated(bytes indexed betID, uint8 indexed country, uint16 indexed category,  bytes betIDData, uint8 league, uint backerStake);
+
+    // Arbitration fee is just added to the steak of the player.
     struct Bet {
         uint256 stakingDeadline;
-        uint256 timeToVote;
+        uint256 deadline; // The same variable used for both deadlines, voting and dispute fee deposit
         uint256 backerStake;
-        uint256 amountStaked;
+        uint256 totalStake;
         uint8 state;
         RulingOptions outcome;
         address payable creator;
         address payable backer;
+        address payable lastArbitrationFeeAddr;
         string description;
         string creatorBetDescription;
     }
@@ -65,7 +71,7 @@ contract GambleBoard is Arbitrable {
      * category: category as integer
      * league: league as integer
      * stakingDeadline: deadline after which no new bets are accepted. Unix time
-     * timeToVote: amount of time to vote after the stakingDeadline. Seconds
+     * deadline: amount of time to vote after the stakingDeadline. Seconds
      * creatorOdd: The odd of the outcome the creator chose. x.yz e 18
      */
     function createBet(string memory description,
@@ -82,7 +88,7 @@ contract GambleBoard is Arbitrable {
         require(creatorOdd > MIN_ODD, "Creator odd has to be bigger than 1!");
         require(country < MAX_COUNTRIES, "Invalid country number!");
         require(stakingDeadline > block.timestamp, "Deadline to place stakes cannot be in the past!");
-        require(timeToVote > MIN_TIME_TO_VOTE, "Time to vote should be atleast 1 day!");
+        require(timeToVote > ONE_DAY, "Time to vote should be atleast 1 day!");
         bytes memory betID = abi.encodePacked(uint160(msg.sender), betsCreated[msg.sender]);
         betsCreated[msg.sender] += 1;
         require(bets[betID].creator == address(0x0), "Bet ID collision!");
@@ -94,15 +100,15 @@ contract GambleBoard is Arbitrable {
         
         uint amountToWinFromBet = (msg.value * creatorOdd) / MIN_ODD;
         newBet.backerStake = amountToWinFromBet - msg.value;
-        newBet.amountStaked = msg.value;
+        newBet.totalStake = msg.value;
 
         newBet.creator = payable(msg.sender);
         newBet.description = description;
         newBet.creatorBetDescription = creatorBetDescription;
         newBet.stakingDeadline = stakingDeadline;
-        newBet.timeToVote = timeToVote;
+        newBet.deadline = timeToVote;
 
-        emit BetCreated(betID, betID, country, category, league, newBet.backerStake);
+        emit BetCreated(betID, country, category, betID, league, newBet.backerStake);
         
         return true;
     }
@@ -121,24 +127,41 @@ contract GambleBoard is Arbitrable {
     }
     
     function claimWinnings(bytes memory betID) public {
-        
+        // If bet is in disputed state and time to deposit arbitration fee has expired,
+        // the first who deposited the fee can claim the winnings.
     }
     
-    function createDispute(bytes memory betID) public {
+    // @title Creates a dispute in the arbitrator contract
+    // Only one player is needed to create the dispute. Arbitration fee goes to the winner.
+    function depositArbitrationFee(bytes memory betID) public payable {
+        Bet storage bet = bets[betID];
+        require(bet.creator != address(0x00), "Bet Does not exist!");
+        require(bet.state == STATE_DISAGREEMENT || bet.state == STATE_DISPUTED, "Bet not in disputed or disagreement state!");
+        require(msg.sender == bet.creator || msg.sender == bet.backer, "Only the players can send the bet to arbitration!");
         
+        if (bet.state == STATE_DISPUTED) {
+            bet.totalStake += msg.value;
+            arbitrator.createDispute{value: msg.value}(RULING_OPTIONS_AMOUNT, "");
+            bet.state = STATE_ARBITRATION;   
+        } else {
+            require(msg.value >= arbitrator.arbitrationCost("0x0"), "Not enough ETH to cover arbitration costs.");
+            bet.totalStake += msg.value;
+            bet.deadline = block.timestamp + ONE_DAY;
+            bet.lastArbitrationFeeAddr = payable(msg.sender);
+            bet.state = STATE_DISPUTED;
+        }
     }
     
     function executeRuling(uint _disputeID, uint _ruling) override internal {
         resolveBet(disputeIDToBetID[_disputeID], _ruling);
-        emit Ruling(arbitrator, _disputeID, _ruling);
     }
     
     //Fallback functions if someone only sends ether to the contract address
     fallback () external payable {
-        revert("Cant send Ether to contract address!");
+        revert("Cant send ETH to contract address!");
     }
     
     receive () external payable {
-        revert("Cant send ether to contract address!");
+        revert("Cant send ETH to contract address!");
     }
 }
